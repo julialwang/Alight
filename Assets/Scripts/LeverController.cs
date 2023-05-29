5eef79361c337c9892fced5333d13aa58b6cad1c
kaeberc <85590549+kaeberc@users.noreply.github.com>
2023-05-29 14:38:25 -0700

    Lever Added (#40)

---
+++ Assets/Scripts/LeverController.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+public class LeverController : MonoBehaviour
+{
+
+    #region Unity
+
+    private void FixedUpdate()
+    {
+        // Reduce movement speed
+        _leverRigidbody.velocity = Vector3.ClampMagnitude(_leverRigidbody.velocity, 0.25f);
+
+        // Edit lever position
+        var leverPosition = _leverTransform.localPosition;
+
+        if (leverPosition.x < -_lever_width/2) {
+                leverPosition.x = -_lever_width/2;
+                _leverTransform.localPosition = leverPosition;
+                _leverRigidbody.velocity = Vector3.zero;
+
+                // Handle press event
+                if (!_isPressed)
+                {
+                    _isPressed = true;
+                    leverPressed.Invoke();
+                }
+            // Button is coming back up
+        } else if (leverPosition.x > _lever_width/2) {
+                leverPosition.x = _lever_width/2;
+                _leverTransform.localPosition = leverPosition;
+                _leverRigidbody.velocity = Vector3.zero;
+
+                // Handle release event
+                if (_isPressed)
+                {
+                    _isPressed = false;
+                    leverReleased.Invoke();
+                }
+        }
+    }
+
+    #endregion
+
+    #region Properties
+
+    public UnityEvent leverPressed;
+    public UnityEvent leverReleased;
+
+    #endregion
+
+    #region Components
+
+    [SerializeField] private Transform _leverTransform;
+    [SerializeField] private Rigidbody _leverRigidbody;
+    [SerializeField] private float _lever_width = 1f;
+    [SerializeField] private bool _isPressed = false;
+
+    #endregion
+}
